6663d2b776745a1e1fb2bb696c84d4ddb970bec4
gouth <gouth@Gowtham>
2024-07-03 15:32:36 +0530

    commit

---
+++ corejava/src/com/tnsif/encapsulation/Demo.java
@@ -0,0 +1,12 @@
+package com.tnsif.encapsulation;
+
+public class Demo {
+	public static void main(String[] args) {
+		Encapsulationdemo d=new Encapsulationdemo();
+		d.serialno=1;
+		d.name="sonu";
+		d.age=21;
+		d.show();
+	}
+
+}
---
+++ corejava/src/com/tnsif/encapsulation/Encapsulationdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulationdemo {
+int serialno;
+String name;
+int age;
+void show()
+{
+	System.out.println("serialno: "+serialno+" name: "+name+" age: "+age);
+}
+}
